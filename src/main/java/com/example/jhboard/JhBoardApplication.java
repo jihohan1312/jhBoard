4ff157c37312d6c0c6121919c5af85a4a9656abe
HanJiHo <han@HanJiHoui-MacBookAir.local>
2021-04-21 14:09:21 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/jhboard/JhBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.jhboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JhBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JhBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jhboard/JhBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jhboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JhBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
